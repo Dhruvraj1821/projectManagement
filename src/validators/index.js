e9f48af9926ad2e8386d2622ef416350a72b13c7
Dhruvraj1821 <dhruvraj18211821@gmail.com>
2026-01-13 19:20:26 +0530

    login implemented and added auth middleware to verify whether the request is valid or not using access token

--- src/validators/index.js
+++ src/validators/index.js
@@ -28,8 +28,21 @@ const userRegisterValidator = () => {
         
 
     ]
-}
+};
+
+const userLoginValidator = () => {
+    return [
+        body("email")
+        .notEmpty()
+        .withMessage("Email is required")
+        .isEmail().withMessage("Email is invalid"),
+        body("password")
+        .notEmpty()
+        .withMessage("Password is required"),
+    ]
+};
 
 export {
-    userRegisterValidator
+    userRegisterValidator,
+    userLoginValidator
 }
